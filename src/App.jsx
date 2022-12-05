88ad6e330036c8cf7c0472b5031c2f7f99ea7c93
Raphael Rocha <raphaelfsrocha@gmail.com>
2022-12-04 23:19:11 -0300

    Crud com data/api

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header";
 import SearchItem from "./SearchItem";
+import apiRequest from "./apiRequest";
 
 function App() {
 	const API_URL = "http://localhost:3500/items";
@@ -33,26 +34,55 @@ function App() {
 		}, 2000);
 	}, []);
 
-	const addItem = (item) => {
+	const addItem = async (item) => {
 		const id = items.length ? items[items.length - 1].id + 1 : 1;
 		const myNewItem = { id, checked: false, item };
 		const listItems = [...items, myNewItem];
 		setItems(listItems);
+
+		const postOptions = {
+			method: "POST",
+			headers: {
+				"Content-Type": " application/json",
+			},
+			body: JSON.stringify(myNewItem),
+		};
+		const result = await apiRequest(API_URL, postOptions);
+		if (result) setFetchError(result);
 	};
 
-	const handleCheck = (id) => {
+	const handleCheck = async (id) => {
 		const listItems = items.map((item) =>
 			item.id === id ? { ...item, checked: !item.checked } : item
 		);
 		setItems(listItems);
+
+		const myItem = listItems.filter((item) => item.id === id);
+		const updateOptions = {
+			method: "PATCH",
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify({ checked: myItem[0].checked }),
+		};
+		const reqUrl = `${API_URL}/${id}`;
+		const result = await apiRequest(reqUrl, updateOptions);
+		if (result) setFetchError(result);
 	};
 
-	const handleDelete = (id) => {
+	const handleDelete = async (id) => {
 		const listItems = items.filter((item) => item.id !== id);
 		setItems(listItems);
 		//shoppinglist variavel no local storage, passa para json
 		// setItems(listItems);
 		// localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+
+		const deleteOptions = {
+			method: "DELETE",
+		};
+		const reqUrl = `${API_URL}/${id}`;
+		const result = await apiRequest(reqUrl, deleteOptions);
+		if (result) setFetchError(result);
 	};
 
 	const handleSubmit = (e) => {
